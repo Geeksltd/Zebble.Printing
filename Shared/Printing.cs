05d02d195666a48bdac3d643e443372996b907b6
mszojaji <ms.zojaji@gmail.com>
2018-06-06 18:53:28 +0430

    Initial commit

---
+++ Shared/Printing.cs
@@ -0,0 +1,23 @@
+﻿namespace Zebble
+{
+    using System;
+    using System.IO;
+
+    public partial class Printing
+    {
+        static FileInfo GetFile(string path)
+        {
+            if (path.IsUrl())
+            {
+                var ext = "png";
+                if (path.ToLower().EndsWith(".webp")) ext = "webp";
+                if (path.ToLower().EndsWithAny(".jpg", "jpeg")) ext = "jpg";
+
+                path = path.ToIOSafeHash();
+                path = Device.IO.Cache.GetFile($"{path}.{ext}").FullName;
+            }
+
+            return Device.IO.File(path);
+        }
+    }
+}
---
+++ iOS/Printing.cs
@@ -0,0 +1,57 @@
+﻿namespace Zebble
+{
+    using Foundation;
+    using System.Threading.Tasks;
+    using UIKit;
+
+    public partial class Printing
+    {
+        /// <summary>
+        /// Print specefic image from a file or an url.
+        /// </summary>
+        /// <param name="path">image file or url</param>
+        public static Task PrintImage(string path)
+        {
+            var imageFile = GetFile(path);
+
+            if (!imageFile.Exists)
+            {
+                Device.Log.Error("The file is not exist");
+                return Task.CompletedTask;
+            }
+
+            var printer = UIPrintInteractionController.SharedPrintController;
+
+            if (printer == null)
+            {
+                Device.Log.Error("Unable to print at this time.");
+            }
+            else
+            {
+                var printInfo = UIPrintInfo.PrintInfo;
+                printInfo.OutputType = UIPrintInfoOutputType.General;
+                printInfo.JobName = "Image is printing";
+
+                printer.PrintInfo = printInfo;
+                printer.PrintingItem = NSUrl.FromFilename(imageFile.FullName);
+                printer.ShowsPageRange = true;
+
+                var handler = new UIPrintInteractionCompletionHandler((printInteractionController, completed, error) =>
+                {
+                    if (completed)
+                    {
+                        Device.Log.Message("Print Completed.");
+                    }
+                    else if (!completed && error != null)
+                    {
+                        Device.Log.Message("Error Printing.");
+                    }
+                });
+
+                printer.Present(true, handler);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
